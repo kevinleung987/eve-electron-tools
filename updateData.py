3a4e3f7de036a125ae9c87a5832aab0bd40af33b
Kevin Leung <kevin.leung987@hotmail.com>
2018-10-11 00:38:08 -0400

    finish data update script

--- updateData.py
+++ updateData.py
@@ -1,26 +1,22 @@
 import requests
 import os
 import bz2
-import csv
+import pandas
 
 downloadsDir = 'downloads/'
+baseUrl = 'https://www.fuzzwork.co.uk/dump/latest/'
 
-fileName = 'invTypes.csv.bz2'
-url = 'https://www.fuzzwork.co.uk/dump/latest/'+fileName
-r = requests.get(url, allow_redirects=True)
-os.makedirs(os.path.dirname(downloadsDir+fileName), exist_ok=True)
-decompressed = str(bz2.decompress(r.content).decode('utf-8'))
-lines = decompressed.split('\n')
-labels = lines[0].split(',')
-print(labels)
-wantedLabels = ['typeID', 'typeName']
-wantedIndexes = []
-for i in range(len(labels)):
-  if labels[i].strip() in wantedLabels:
-    wantedIndexes.append(i)
-print(wantedIndexes)
 
-items = decompressed.split(',')
-print(items[:len(labels)])
+def downloadData(fileName, columns):
+    os.makedirs(os.path.dirname(downloadsDir +
+                                fileName + '.bz2'), exist_ok=True)
+    r = requests.get(baseUrl + fileName + '.bz2', allow_redirects=True)
+    open(downloadsDir + fileName + '.FULL',
+         'wb').write(bz2.decompress(r.content))
 
-# open(downloadsDir+'invTypes.csv', 'wb').write(output)
+    df = pandas.read_csv(downloadsDir + fileName + '.FULL', usecols=columns)
+    df.to_csv(downloadsDir + fileName, index=False)
+    os.remove(downloadsDir + fileName + '.FULL')
+
+
+downloadData('invTypes.csv', ['typeID', 'typeName'])
