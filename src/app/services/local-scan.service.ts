7a4adc4f4fefc2de0eb62c9e1db23c5e66300db1
kevinleung987 <kevin.leung987@hotmail.com>
2018-10-12 18:12:09 -0400

    convert local-scan from subscription to async/await structure

--- src/app/services/local-scan.service.ts
+++ src/app/services/local-scan.service.ts
@@ -56,55 +56,52 @@ export class LocalScanService {
     const lines = localList.split('\n');
     for (let i = 0; i < lines.length; i++) {
       // Search the character in ESI
-      from(this.eve.search(lines[i], 'character', true))
-        .subscribe(searchData => {
-          // If the character exists in ESI
-          if (searchData['character']) {
-            const id = searchData['character'][0];
-            if (!this.characters[id]) {
-              // Find the character's id and corporation
-              from(this.eve.characters(id))
-                .subscribe(charData => {
-                  this.characters[id] = {
-                    name: lines[i],
-                    corporation: charData['corporation_id']
-                  };
-                  const corpId = charData['corporation_id'];
-                  if (this.corporations[corpId]) {
-                    this.addDisplayCorporation(corpId);
-                  } else {
-                    from(this.eve.corporations(corpId))
-                      .subscribe(corpData => {
-                        this.corporations[corpId] = {
-                          name: corpData['name'],
-                          alliance: corpData['alliance_id'] || null,
-                          image: `http://image.eveonline.com/Corporation/${corpId}_128.png`
-                        };
-                        this.addDisplayCorporation(corpId);
-                        if (corpData['alliance_id']) {
-                          from(this.eve.alliances(corpData['alliance_id']))
-                            .subscribe(allianceData => {
-                              if (this.alliances[corpData['alliance_id']]) {
-                                this.addDisplayAlliance(corpData['alliance_id']);
-                              } else {
-                                this.alliances[corpData['alliance_id']] = {
-                                  name: allianceData['name'],
-                                  corporations: null,
-                                  image: `http://image.eveonline.com/Alliance/${corpData['alliance_id']}_128.png`
-                                };
-                                this.addDisplayAlliance(corpData['alliance_id']);
-                              }
-                            });
-                        }
-                      });
-                  }
-                });
-            } else { // If the character already exists in our cache
-              this.addDisplayCorporation(this.characters[id].corporation);
-              this.addDisplayAlliance(this.corporations[this.characters[id].corporation].alliance);
+      const searchData: any = await this.eve.search(lines[i], 'character', true);
+      // If the character exists in ESI
+      if (searchData['character']) {
+        const id = searchData['character'][0];
+        if (!this.characters[id]) {
+          // Find the character's id and corporation
+          const charData: any = await this.eve.characters(id);
+          this.characters[id] = {
+            name: lines[i],
+            corporation: charData['corporation_id']
+          };
+          const corpId = charData['corporation_id'];
+          if (this.corporations[corpId]) {
+            this.addDisplayCorporation(corpId);
+          } else {
+            // Get character's corporation data
+            const corpData: any = await this.eve.corporations(corpId);
+            this.corporations[corpId] = {
+              name: corpData['name'],
+              alliance: corpData['alliance_id'] || null,
+              image: `http://image.eveonline.com/Corporation/${corpId}_128.png`
+            };
+            this.addDisplayCorporation(corpId);
+            if (corpData['alliance_id']) {
+              // Get character's alliance data
+              const allianceData: any = await this.eve.alliances(corpData['alliance_id']);
+              if (this.alliances[corpData['alliance_id']]) {
+                this.addDisplayAlliance(corpData['alliance_id']);
+              } else {
+                this.alliances[corpData['alliance_id']] = {
+                  name: allianceData['name'],
+                  corporations: null,
+                  image: `http://image.eveonline.com/Alliance/${corpData['alliance_id']}_128.png`
+                };
+                this.addDisplayAlliance(corpData['alliance_id']);
+              }
             }
           }
-        });
+        } else {
+          // If the character already exists in our cache
+          this.addDisplayCorporation(this.characters[id].corporation);
+          this.addDisplayAlliance(
+            this.corporations[this.characters[id].corporation].alliance
+          );
+        }
+      }
     }
   }
 }
