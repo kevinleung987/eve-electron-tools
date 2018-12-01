cf43f8af7b9a4140e6de6b883cd49e1cac772be0
Kevin Leung <kevin.leung987@hotmail.com>
2018-12-01 15:51:37 -0500

    fix hot-reload

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
-const {
-  app,
-  BrowserWindow
-} = require("electron");
+const { app, BrowserWindow } = require("electron");
 const path = require("path");
 const url = require("url");
 
-let win;
+let win, serve;
+const args = process.argv.slice(1);
+serve = args.some(val => val === '--serve');
 
 function createWindow() {
+
   win = new BrowserWindow({
     width: 1600,
     height: 900
   });
 
-  // load the dist folder from Angular
-  win.loadURL(
-    url.format({
-      pathname: path.join(__dirname, `/dist/index.html`),
-      protocol: "file:",
-      slashes: true
-    })
-  );
+  if (serve) { // load from web server
+    require('electron-reload')(__dirname, {
+      electron: require(`${__dirname}/node_modules/electron`)
+    });
+    win.loadURL('http://localhost:4200');
+  } else { // load the dist folder from Angular
+    win.loadURL(
+      url.format({
+        pathname: path.join(__dirname, `/dist/index.html`),
+        protocol: "file:",
+        slashes: true
+      })
+    );
+  }
 
   // The following is optional and will open the DevTools:
   //win.webContents.openDevTools()
@@ -29,19 +35,23 @@ function createWindow() {
     win = null;
   });
 }
+try {
+  app.on("ready", createWindow);
 
-app.on("ready", createWindow);
+  // on macOS, closing the window doesn't quit the app
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
+  });
 
-// on macOS, closing the window doesn't quit the app
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
+  // initialize the app's main window
+  app.on("activate", () => {
+    if (win === null) {
+      createWindow();
+    }
+  });
+} catch (e) {
+  throw e;
+}
 
-// initialize the app's main window
-app.on("activate", () => {
-  if (win === null) {
-    createWindow();
-  }
-});
