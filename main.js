043a616f06b9c0718d636034b261afc088d4d1d2
Kevin Leung <kevin.leung987@hotmail.com>
2018-11-30 22:29:36 -0500

    streamline postinstall, fix main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function createWindow() {
   );
 
   // The following is optional and will open the DevTools:
-  win.webContents.openDevTools()
+  //win.webContents.openDevTools()
 
   win.on("closed", () => {
     win = null;
--- postinstall.js
+++ postinstall.js
@@ -4,10 +4,13 @@ const f_angular = 'node_modules/@angular-devkit/build-angular/src/angular-cli-fi
 fs.readFile(f_angular, 'utf8', function (err, data) {
   if (err) {
     return console.log(err);
+  } else if (process.argv.length != 3) {
+    return console.error('You must specify web or electron-renderer');
   }
+  var target = process.argv[2];
   var result = data.replace(/target: "electron-renderer",/g, '');
   var result = result.replace(/target: "web",/g, '');
-  var result = result.replace(/return \{/g, 'return {target: "electron-renderer",');
+  var result = result.replace(/return \{/g, 'return {target: "' + target + '",');
 
   fs.writeFile(f_angular, result, 'utf8', function (err) {
     if (err) return console.log(err);
