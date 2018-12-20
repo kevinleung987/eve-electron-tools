98a5692132df2d8ebd24b6eab23cef0537342636
Kevin Leung <kevin.leung987@hotmail.com>
2018-12-20 14:31:13 -0500

    add ImagePipe

---
+++ src/app/common/image.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'image' })
+export class ImagePipe implements PipeTransform {
+  constructor() { }
+
+  transform(value: any, valueType: any): string {
+    switch (valueType) {
+      case 'character':
+        return `https://image.eveonline.com/Character/${value}_64.jpg`;
+      case 'corporation':
+        return `https://image.eveonline.com/Corporation/${value}_64.png`;
+      case 'alliance':
+        return `https://image.eveonline.com/Alliance/${value}_64.png`;
+      case 'ship':
+        return `https://imageserver.eveonline.com/Render/${value}_64.png`;
+      default:
+        return null;
+    }
+  }
+}
