c84d55bf79820f20c299fd64aeba99400d822503
Kevin Leung <kevin.leung987@hotmail.com>
2018-12-24 17:50:50 -0500

    Implement flexible Autocomplete in Market tool

--- src/app/tools/market/market.component.ts
+++ src/app/tools/market/market.component.ts
@@ -3,6 +3,7 @@ import { UniverseService } from 'src/app/services/universe.service';
 import { MatchService } from 'src/app/services/match.service';
 import { fromEvent } from 'rxjs';
 import { map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { AlertService } from 'src/app/services/alert.service';
 
 
 @Component({
@@ -11,46 +12,73 @@ import { map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
   styleUrls: ['./market.component.scss']
 })
 export class MarketComponent implements OnInit {
+  readonly numSuggestions = 10;
+  readonly searchDelay = 200;
   items = [];
   searchValue = '';
   activeIndex = -1;
+  dirty = false;
+
   @ViewChild('searchBar') searchBar: ElementRef;
-  constructor(public universe: UniverseService, public match: MatchService) { }
+  constructor(private alert: AlertService, public universe: UniverseService, public match: MatchService) { }
 
   ngOnInit() {
+    // If the user does not type for a certain amount of time, refresh the autocomplete suggestions
     fromEvent(this.searchBar.nativeElement, 'input')
       .pipe(map((event: Event) => (<HTMLInputElement>event.target).value),
-        debounceTime(200),
+        debounceTime(this.searchDelay),
         distinctUntilChanged()
-      ).subscribe(data => this.search());
+      ).subscribe(data => {
+        this.activeIndex = -1;
+        this.search();
+        this.dirty = false;
+      });
   }
 
-  search() {
-    this.items = this.match.autoComplete(this.match.typeNames, this.searchValue, 10);
-  }
-
-  activate(index) {
-    this.activeIndex = index;
-    this.searchValue = this.items[this.activeIndex];
+  makeDirty() {
+    this.dirty = true;
   }
 
   onKeydown(event: KeyboardEvent) {
-    if (event.key === 'ArrowDown') {
+    // Manipulate index pointing at the active suggestion
+    if (event.key === 'ArrowDown' || event.key === 'Tab') {
       if (this.activeIndex < this.items.length - 1) {
-        this.activeIndex++;
+        this.activate(this.activeIndex + 1);
+      } else if (this.items.length > 0) {
+        this.activate(0);
       }
+      event.preventDefault();
+      event.stopPropagation();
     } else if (event.key === 'ArrowUp') {
       if (this.activeIndex > 0) {
-        this.activeIndex--;
+        this.activate(this.activeIndex - 1);
+      } else if (this.items.length > 0) {
+        this.activate(this.items.length - 1);
       }
     } else if (event.key === 'Enter' || event.key === 'ArrowRight') {
-      this.activate(this.activeIndex);
+      // Treat as if Submit button had been pressed
+      this.onSubmit();
       event.preventDefault();
       event.stopPropagation();
     }
   }
 
-  submit() {
-    console.log(this.searchValue);
+  search() {
+    this.items = this.match.autoComplete(this.match.typeNames, this.searchValue, this.numSuggestions);
+  }
+
+  activate(index: number) {
+    if (this.items.length === 0) { return; }
+    this.activeIndex = index >= 0 ? index : 0;
+    this.searchValue = this.items[this.activeIndex];
+  }
+
+  onSubmit() {
+    if (this.dirty || this.items.length === 0) { return; }
+    if (this.items.length === 0) { this.alert.warning('No items found.'); return; }
+    this.activate(this.activeIndex);
+    const submission = this.universe.getTypeId(this.searchValue);
+    if (submission == null) { throw new Error('Bad search value submitted.'); }
+    this.alert.info(`${this.searchValue}[${submission.toString()}]`);
   }
 }
