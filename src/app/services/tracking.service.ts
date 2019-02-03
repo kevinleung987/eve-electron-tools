b87a73f02bb661d70729ef8878f93ee060328912
Kevin Leung <kevin.leung987@hotmail.com>
2019-02-02 21:21:19 -0500

    Remove electron-google-analytics | Too buggy for production

--- src/app/services/tracking.service.ts
+++ src/app/services/tracking.service.ts
@@ -1,20 +1,11 @@
 import { Injectable } from '@angular/core';
-import Analytics from 'electron-google-analytics';
-import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class TrackingService {
-  analytics = new Analytics(environment.tracking, { debug: environment.production, version: environment.version });
+  analytics = null;
   path = '/';
   clientID: string;
   constructor() {
-    this.analytics.pageview('EVE Electron Tools', this.path, 'Page View', undefined)
-      .then((response) => {
-        this.clientID = response.clientID;
-        console.log(this.clientID);
-        return response;
-      }).catch((err) => {
-        return err;
-      });
+
   }
 }
