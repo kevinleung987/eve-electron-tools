d6d760efe3fde6c697fd0f506b74649c225bac34
Kevin Leung <kevin.leung987@hotmail.com>
2019-02-09 13:07:43 -0500

    Add page-view tracking

--- src/app/services/tracking.service.ts
+++ src/app/services/tracking.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class TrackingService {
-  analytics = null;
-  path = '/';
   clientID: string;
-  constructor() {
-
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        (<any>window).gtag('config', environment.tracking, {
+          'page_title': event.urlAfterRedirects.substring(1, event.urlAfterRedirects.length),
+          'page_path': event.urlAfterRedirects
+        });
+      }
+    });
   }
 }
