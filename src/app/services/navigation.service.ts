e121f5e9ed61add0ec8f0492e1ab5f83cf6c27f0
Kevin Leung <kevin.leung987@hotmail.com>
2019-02-16 16:56:28 -0500

    Refactor Navigation Service

--- src/app/services/navigation.service.ts
+++ src/app/services/navigation.service.ts
@@ -3,23 +3,23 @@ import { Injectable } from '@angular/core';
 import { Papa, PapaParseResult } from 'ngx-papaparse';
 import { BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
-
-import { UniverseService } from './universe.service';
+import { UniverseService } from 'src/app/services/universe.service';
+import { PriorityQueue } from 'src/app/util/priority-queue';
 
 @Injectable({ providedIn: 'root' })
 export class NavigationService {
   fileName = 'mapSolarSystemJumps.csv';
-  jumps = {};
+  jumps = {}; // Mapping each system with it's connected systems in a graph
   ready = new BehaviorSubject(false);
 
   constructor(private http: HttpClient, private papa: Papa, private universe: UniverseService) {
+    // Build solar system graph
     this.http.get('./assets/' + this.fileName, { responseType: 'text' })
       .pipe(map((data: any) => data))
       .subscribe((data: any) => {
         this.papa.parse(data, {
           header: true,
           complete: (result: PapaParseResult) => {
-            // Map other columns as fields for the specified key
             const tempData = {};
             result.data.forEach(element => {
               if (tempData[element['fromSolarSystemID']]) {
@@ -33,10 +33,6 @@ export class NavigationService {
           }
         });
       }, error => console.error(error));
-    this.ready.subscribe((result) => {
-      if (result) {
-      }
-    });
   }
 
   getRoute(start: number, finish: number, costFunc: (id: number) => number) {
@@ -97,80 +93,7 @@ export class NavigationService {
     const x = aData['x'] - bData['x'];
     const y = aData['y'] - bData['y'];
     const z = aData['z'] - bData['z'];
+    // Conversion to Light Years
     return Math.sqrt(x * x + y * y + z * z) / 9460700000000000;
-  }
-}
-
-/**
- * Priority queue used in A* search
- */
-class PriorityQueue {
-  _comparator;
-  _heap;
-  top = 0;
-  parent = i => ((i + 1) >>> 1) - 1;
-  left = i => (i << 1) + 1;
-  right = i => (i + 1) << 1;
-
-  constructor(comparator = (a, b) => a < b) {
-    this._heap = [];
-    this._comparator = comparator;
-  }
-  size() {
-    return this._heap.length;
-  }
-  isEmpty() {
-    return this.size() === 0;
-  }
-  peek() {
-    return this._heap[this.top];
-  }
-  push(...values) {
-    values.forEach(value => {
-      this._heap.push(value);
-      this._siftUp();
-    });
-    return this.size();
-  }
-  pop() {
-    const poppedValue = this.peek();
-    const bottom = this.size() - 1;
-    if (bottom > this.top) {
-      this._swap(this.top, bottom);
-    }
-    this._heap.pop();
-    this._siftDown();
-    return poppedValue;
-  }
-  replace(value) {
-    const replacedValue = this.peek();
-    this._heap[this.top] = value;
-    this._siftDown();
-    return replacedValue;
-  }
-  _greater(i, j) {
-    return this._comparator(this._heap[i], this._heap[j]);
-  }
-  _swap(i, j) {
-    [this._heap[i], this._heap[j]] = [this._heap[j], this._heap[i]];
-  }
-  _siftUp() {
-    let node = this.size() - 1;
-    while (node > this.top && this._greater(node, this.parent(node))) {
-      this._swap(node, this.parent(node));
-      node = this.parent(node);
-    }
-  }
-  _siftDown() {
-    let node = this.top;
-    while (
-      (this.left(node) < this.size() && this._greater(this.left(node), node)) ||
-      (this.right(node) < this.size() && this._greater(this.right(node), node))
-    ) {
-      const maxChild =
-        (this.right(node) < this.size() && this._greater(this.right(node), this.left(node))) ? this.right(node) : this.left(node);
-      this._swap(node, maxChild);
-      node = maxChild;
-    }
   }
 }
